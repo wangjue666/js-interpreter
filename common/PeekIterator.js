84180cb7db5aad2d66681ae840e7a65c6b92d570
王珏 <wangjuesix@gmail.com>
2020-12-02 11:37:28 +0800

    实现流的peek和pubback操作

---
+++ common/PeekIterator.js
@@ -0,0 +1,54 @@
+const LinkedList = require('linkedlist')
+
+const CACHE_SIZE = 10
+
+class PeekIterator {
+    constructor(it, endToken = null) {
+        this.id = it
+        // 需要putBack的元素
+        this.stackPutBacks = new LinkedList()
+        // 基于时间窗口的缓存
+        this.queueCache = new LinkedList()
+        this.endToken = endToken
+    }
+
+    peek(){
+        if(this.stackPutBacks.length > 0){
+            return this.stackPutBacks.head
+        }
+        const val = this.next()
+        this.pubBack()
+        return val
+    }
+    pubBack(){
+        if(this.queueCache.length > 0){
+            this.stackPutBacks.push(this.queueCache.pop())
+        }
+    }
+
+    hasNext(){
+        return this.endToken || Boolean(this.peek())
+    }
+
+    next(){
+        let val = null
+        if(this.stackPutBacks.length > 0){
+            val = this.stackPutBacks.pop()
+        }else{
+            val = this.it.next().value
+            if(val === undefined){
+                const tmp = this.endToken
+                this.endToken = null
+                return tmp
+            }
+        }
+        // 处理缓存
+        while(this.queueCache.length >=  CACHE_SIZE){
+            this.queueCache.shift()
+        }
+        this.queueCache.push(val)
+        return val
+    }
+}
+
+module.exports = PeekIterator
