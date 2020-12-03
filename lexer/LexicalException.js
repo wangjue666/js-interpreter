371d5e0f72411c6bfec05c3e2b15ff1180e65c1e
王珏 <wangjuesix@gmail.com>
2020-12-03 12:23:48 +0800

    定义词法分析异常处理的类

---
+++ lexer/LexicalException.js
@@ -0,0 +1,10 @@
+// 异常处理的类
+
+class LexicalException extends Error {
+    constructor(msg){
+        super(msg)
+    }
+    static fromChar(c){
+        return new LexicalException(`unexprected char ${c}`)
+    }
+}
